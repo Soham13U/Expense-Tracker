6b6ee0d292f957d5df3526356e5dc0b2b5df4c60
Soham <sohamup13@gmail.com>
2022-03-01 12:17:52 +0530

    Initial Commit

---
+++ expenseTracker/authentication/urls.py
@@ -0,0 +1,7 @@
+from unicodedata import name
+from .views import RegistrationView
+from  django.urls import path
+
+urlpatterns = [
+    path('register',RegistrationView.as_view(),name='register')
+]
