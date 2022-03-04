1c4ff1b14dc5099df58c6ef4906c1d9f52486d19
Soham <sohamup13@gmail.com>
2022-03-04 10:43:17 +0530

    deployed to heroku

--- expenseTracker/expenseTracker/settings.py
+++ expenseTracker/expenseTracker/settings.py
@@ -1,4 +1,6 @@
 import os
+import django_heroku
+
 """
 Django settings for expenseTracker project.
 
@@ -132,6 +134,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'expenseTracker/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
