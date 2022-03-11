077b2e76ae37c62075821090c723bbe5c39c1a38
Soham <sohamup13@gmail.com>
2022-03-11 16:10:10 +0530

    login/logout

--- expenseTracker/authentication/urls.py
+++ expenseTracker/authentication/urls.py
@@ -1,5 +1,5 @@
 from unicodedata import name
-from .views import EmailValidationView, RegistrationView,UsernameValidationView,EmailValidationView,VerificationView,LoginView
+from .views import EmailValidationView, RegistrationView,UsernameValidationView,EmailValidationView,VerificationView,LoginView,LogoutView
 from  django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('validate-username',csrf_exempt(UsernameValidationView.as_view()),name="validate-username"),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate_email'),
     path('activate/<uidb64>/<token>',VerificationView.as_view(),name='activate'),
-    path('login',LoginView.as_view(),name='login')
+    path('login',LoginView.as_view(),name='login'),
+    path('logout',LogoutView.as_view(),name='logout')
 ]
