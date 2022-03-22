cd041fa5ffc68939502c94a91d46bb85b1869c01
Soham <sohamup13@gmail.com>
2022-03-22 15:18:59 +0530

    search

--- expenseTracker/expenses/views.py
+++ expenseTracker/expenses/views.py
@@ -15,12 +15,23 @@ from django.core.paginator import Paginator
 
 from .models import Category
 
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
 # Create your views here.
 @login_required(login_url='/authentication/login')
 def index(request):
     categories=Category.objects.all()
     expenses=Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses,2)
+    paginator = Paginator(expenses,5)
     page_number =request.GET.get('page')
     page_obj = Paginator.get_page(paginator,page_number)
    
