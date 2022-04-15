88d3a58fde2cf5014d336579b2228d35c3988145
Soham <sohamup13@gmail.com>
2022-04-15 15:43:05 +0530

    commit three

--- expenseTracker/expenseTracker/settings.py
+++ expenseTracker/expenseTracker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&j#5(6jcal-a7_e#t5+kwu97xk1k7px&b+!!!7^%1n$u%wxz6&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
