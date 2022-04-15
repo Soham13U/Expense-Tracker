22f751172ac1f3b80affbb41ef2dd3b293f2cf20
Soham <sohamup13@gmail.com>
2022-04-15 15:30:31 +0530

    commit two

--- expenseTracker/expenseTracker/settings.py
+++ expenseTracker/expenseTracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&j#5(6jcal-a7_e#t5+kwu97xk1k7px&b+!!!7^%1n$u%wxz6&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
