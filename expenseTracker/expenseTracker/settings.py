1fe95dc972b0a06e87011b5ce47494ae3c6efe39
Soham <sohamup13@gmail.com>
2022-04-15 17:12:18 +0530

    Improved UI and added icons

--- expenseTracker/expenseTracker/settings.py
+++ expenseTracker/expenseTracker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&j#5(6jcal-a7_e#t5+kwu97xk1k7px&b+!!!7^%1n$u%wxz6&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
